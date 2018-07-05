f75029c15f3f72f934703fdca7cd672a37b233fa
Maksim Galanin <m.galanin@milofon.pro>
2018-07-05 12:42:00 +0300

    add package for rpc

---
+++ service/dango/service/protocol/rpc/doc.d
@@ -0,0 +1,25 @@
+/**
+ * Модуль реализации системы документирования RPC
+ *
+ * Copyright: (c) 2015-2017, Milofon Project.
+ * License: Subject to the terms of the BSD license, as written in the included LICENSE.txt file.
+ * Author: <m.galanin@milofon.org> Maksim Galanin
+ * Date: 2018-07-05
+ */
+
+module dango.service.protocol.rpc.doc;
+
+private
+{
+}
+
+
+/**
+ * Аннотация определяет документацию для обработчика команды
+ */
+struct Doc
+{
+    string content;
+}
+
+
---
+++ service/dango/service/protocol/rpc/package.d
@@ -0,0 +1,15 @@
+/**
+ * Copyright: (c) 2015-2017, Milofon Project.
+ * License: Subject to the terms of the BSD license, as written in the included LICENSE.txt file.
+ * Author: <m.galanin@milofon.org> Maksim Galanin
+ * Date: 2018-07-05
+ */
+
+module dango.service.protocol.rpc;
+
+public
+{
+    import dango.service.protocol.rpc.doc;
+    import dango.service.protocol.rpc.controller;
+}
+
